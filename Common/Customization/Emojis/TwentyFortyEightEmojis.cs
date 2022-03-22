49a92771b38a7ab827ac9bf8d13179ccc50778ef
Никита Газиев <zobweyt@gmail.com>
2022-03-22 11:54:56 +0300

    New directory with custom emojis

---
+++ Common/Customization/Emojis/TwentyFortyEightEmojis.cs
@@ -0,0 +1,18 @@
+﻿namespace Gudgeon.Common.Customization.TwentyFortyEight
+{
+    internal static class TwentyFortyEightEmojis
+    {
+        public const string T0 = "<:2048_0:947385479379161128>";
+        public const string T2 = "<:2048_2:947386011963502602>";
+        public const string T4 = "<:2048_4:947387437422899220>";
+        public const string T8 = "<:2048_8:947388568240787506>";
+        public const string T16 = "<:2048_16:947395394390880256>";
+        public const string T32 = "<:2048_32:947394840746917928>";
+        public const string T64 = "<:2048_64:947394523477209138>";
+        public const string T128 = "<:2048_128:947395868561125387>";
+        public const string T256 = "<:2048_256:947396415066341427>";
+        public const string T512 = "<:2048_512:947396754343616512>";
+        public const string T1024 = "<:2048_1024:947397244271865877>";
+        public const string T2048 = "<:2048_2048:947397509913907220>";
+    }
+}
