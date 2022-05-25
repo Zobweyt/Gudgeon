9bb9e8223fd5fa69199d1189a9a2cb9d0dc0c3dd
Никита Газиев <zobweyt@gmail.com>
2022-05-25 14:55:42 +0300

    Added UlongTypeReader.

---
+++ TypeReaders/UlongTypeReader.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Gudgeon.TypeReaders
+{
+    internal sealed class UlongTypeReader : TypeReader<ulong>
+    {
+        public override Task<TypeConverterResult> ReadAsync(IInteractionContext context, string option, IServiceProvider services)
+        {
+            if (ulong.TryParse(option, out ulong result))
+            {
+                return Task.FromResult(TypeConverterResult.FromSuccess(result));
+            }
+
+            return Task.FromResult(TypeConverterResult.FromError(InteractionCommandError.ConvertFailed, $"Value **{option}** cannot be converted to **{typeof(ulong)}**"));
+        }
+    }
+}
