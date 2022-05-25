f1417a45c80295484322135d8d0a2de3795a5804
Никита Газиев <zobweyt@gmail.com>
2022-05-25 14:55:58 +0300

    Added UlongTypeConverter.

---
+++ TypeConverters/UlongTypeConverter.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Gudgeon.TypeConverters
+{
+    internal sealed class UlongTypeConverter : TypeConverter<ulong>
+    {
+        public override ApplicationCommandOptionType GetDiscordType() => ApplicationCommandOptionType.String;
+        public override Task<TypeConverterResult> ReadAsync(IInteractionContext context, IApplicationCommandInteractionDataOption option, IServiceProvider services)
+        {
+            if (ulong.TryParse((string)option.Value, out ulong result))
+            {
+                return Task.FromResult(TypeConverterResult.FromSuccess(result));
+            }
+    
+            return Task.FromResult(TypeConverterResult.FromError(InteractionCommandError.ConvertFailed, $"Value **{option.Value}** cannot be converted to **{typeof(ulong)}**"));
+        }
+    }
+}
