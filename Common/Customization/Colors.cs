29133c6e382cf021483debacf92493ecbcbc2bd6
Никита Газиев <zobweyt@gmail.com>
2022-05-25 14:53:59 +0300

    Added Discord color.

--- Common/Customization/Colors.cs
+++ Common/Customization/Colors.cs
@@ -4,7 +4,8 @@ namespace Gudgeon.Common.Customization
 {
     internal static class Colors
     {
-        public static readonly Color Default = new(250, 250, 250);
+        public static readonly Color Default = new(240, 240, 240);
+        public static readonly Color Discord = new(88, 101, 242);
         public static readonly Color Success = new(43, 182, 115);
         public static readonly Color Error = new(231, 76, 60);
     }
