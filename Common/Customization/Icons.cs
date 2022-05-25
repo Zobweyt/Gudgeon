f7fadb345d3a42ef28c30c4a2beb098a65727ba9
Никита Газиев <zobweyt@gmail.com>
2022-05-25 14:54:09 +0300

    Added Garbage icon.

--- Common/Customization/Icons.cs
+++ Common/Customization/Icons.cs
@@ -4,5 +4,6 @@
     {
         public const string Success = "https://icons-for-free.com/iconfiles/png/512/complete+done+green+success+valid+icon-1320183462969251652.png";
         public const string Error = "https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-error-icon.png";
+        public const string Garbage = "https://icons-for-free.com/iconfiles/png/512/can+general+office+recycle+bin+rubbish+bin+trash+bin+trash-1320183168955020811.png";
     }
 }
